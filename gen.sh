619d0f77d7d2887fd696f7089d6f972c445e2624
ld3z <ldeziangamer@gmail.com>
2023-01-13 23:54:58 -0700

    reset

--- gen.sh
+++ gen.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #header
-printf '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n\n<channel>\n<title>9anime - RSS Feed</title>\n<link>https://github.com/ld3z/anime-rss</link>\n<description>A simple RSS feed for 9anime!</description>\n' > anime.xml
+printf '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n\n<channel>\n<title>Allanime (Sub) - RSS Feed</title>\n<link>https://github.com/coolnsx/anime-rss</link>\n<description>A simple RSS feed for allanime!</description>\n' > allanime.xml
 
 #content
-for i in $(curl -s "https://9anime.to/updated" -A uwu | sed -nE 's|.*href="(/watch/[^"]*)" class.*|\1|p')
+for i in $(curl -s "https://allanime.site/anime" -A uwu | sed -nE 's|.*href="(/watch/[^"]*)" class.*|\1|p')
 do
-	grep -q "$i" anime.xml || printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for 9anime!</description>\n</item>\n' "$(printf "%s" "$i" | cut -d'/' -f4- | tr '[:punct:]' ' ' )" "https://9anime.to${i}" >> anime.xml
+	grep -q "$i" allanime.xml || printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for allanime!</description>\n</item>\n' "$(printf "%s" "$i" | cut -d'/' -f4- | tr '[:punct:]' ' ' )" "https://allanime.site${i}" >> allanime.xml
 done
 
 #footer
-printf "\n</channel>\n</rss>" >> anime.xml
+printf "\n</channel>\n</rss>" >> allanime.xml
