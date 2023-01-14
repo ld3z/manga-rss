d43e582c1b200c0443aff49346847e554af78c19
ld3z <ldeziangamer@gmail.com>
2023-01-13 23:53:45 -0700

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -6,7 +6,7 @@ printf '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n\n<channel
 #content
 for i in $(curl -s "https://9anime.to/updated" -A uwu | sed -nE 's|.*href="(/watch/[^"]*)" class.*|\1|p')
 do
-	grep -q "$i" anime.xml || printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for 9anime!</description>\n</item>\n' "$(printf "%s" "$i" | cut -d'/' -f4- | tr '[:punct:]' ' ' )" "https://allanime.site${i}" >> anime.xml
+	grep -q "$i" anime.xml || printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for 9anime!</description>\n</item>\n' "$(printf "%s" "$i" | cut -d'/' -f4- | tr '[:punct:]' ' ' )" "https://9anime.to${i}" >> anime.xml
 done
 
 #footer
