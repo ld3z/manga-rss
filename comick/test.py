9e3ebe959e63a406051526c7723d08c5138138ad
ld3z <ldeziangamer@gmail.com>
2023-01-14 01:26:30 -0700

    testr

--- comick/test.py
+++ comick/test.py
@@ -3,22 +3,19 @@ import json
 import re
 import os
 
-
 def get_latest():
     link = 'https://api.comick.app/chapter/?page=1&order=new&accept_mature_content=true'
-
     response = requests.get(link, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"}).json()
+    data = response['data']
+    chapters = []
+    for item in data:
+        title = item.get('md_comics').get('title')
+        slug = item.get('md_comics').get('slug')
+        chapter = item.get('chap')
+        chapters.append([slug, chapter, title])
+    return chapters
 
-    return [
-        [
-            '{}'.format(x['slug']),
-            x['chap'],
-            x['title']
-        ] for x in response['data']
-    ]
-
-
-def generate_rss():
+def generate_rss(chapters):
     rss = f"""
 <?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
@@ -29,7 +26,7 @@ def generate_rss():
 <description>A simple RSS feed for ComicK!</description>
 """
 
-    for item in get_latest():
+    for item in chapters:
         rss += """
 <item>
     <title>{}</title>
@@ -42,10 +39,14 @@ def generate_rss():
     return rss
 
 try:
+    chapters = get_latest()
+    print(chapters)
+    rss = generate_rss(chapters)
     filename = f'./comick/comick-rss.xml'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     if os.path.exists(filename):
         os.remove(filename)
     with open(filename, 'w') as f:
-        f.write(generate_rss().strip())
+        f.write(rss.strip())
 except:
     print('Comick failed.')
