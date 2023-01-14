020411d79c628b7b907a47eb6983d9a28dd57d64
ld3z <ldeziangamer@gmail.com>
2023-01-13 23:49:52 -0700

    test

---
+++ gen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#header
+printf '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n\n<channel>\n<title>9anime - RSS Feed</title>\n<link>https://github.com/ld3z/anime-rss</link>\n<description>A simple RSS feed for 9anime!</description>\n' > anime.xml
+
+#content
+for i in $(curl -s "https://9anime.to/updated" -A uwu | sed -nE 's|.*href="(/watch/[^"]*)" class.*|\1|p')
+do
+	grep -q "$i" anime.xml || printf '\n<item>\n<title>%s</title>\n<link>%s</link>\n<description>A simple RSS feed for 9anime!</description>\n</item>\n' "$(printf "%s" "$i" | cut -d'/' -f4- | tr '[:punct:]' ' ' )" "https://allanime.site${i}" >> anime.xml
+done
+
+#footer
+printf "\n</channel>\n</rss>" >> anime.xml
