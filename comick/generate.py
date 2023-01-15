403d109b8dc29923efbb288960d33ba7bbf6afc1
ld3z <ldeziangamer@gmail.com>
2023-01-15 14:29:52 -0700

    fix everything

--- comick/generate.py
+++ comick/generate.py
@@ -1,25 +1,8 @@
 import requests
-import json
-import re
-import os
 
 
-def get_latest():
-    link = 'https://api.comick.app/chapter/?page=1&order=new&accept_mature_content=true'
-
-    response = requests.get(link, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"}).json()
-
-    return [
-        [
-            '{}'.format(x['slug']),
-            x['chap'],
-            x['title']
-        ] for x in response['data']
-    ]
-
-
-def generate_rss():
-    rss = f"""
+def generate_rss(data):
+    rss = """\
 <?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 
@@ -29,23 +12,27 @@ def generate_rss():
 <description>A simple RSS feed for ComicK!</description>
 """
 
-    for item in get_latest():
-        rss += """
+    for i in data:
+        c = i['md_comics']
+
+        rss += """\
 <item>
     <title>{}</title>
     <link>{}</link>
-    <description>{} by {}</description>
+    <description>{}</description>
 </item>
-""".format(f"{item[2]} - Chapter {item[1]}", "https://comick.app/comic/{}".format(item[0]), f"Chapter {item[1]} of {item[2]} is now available on ComicK!")
+""".format(
+            f"{c['title']} - Chapter {i['chap']}",
+            f"https://comick.app/comic/{c['slug']}",
+            f"Chapter {i['chap']} of {c['title']} is now available on ComicK!",
+        )
 
-    rss += '\n</channel>\n</rss>'
+    rss += "\n</channel>\n</rss>"
     return rss
 
-try:
-    filename = f'./comick/comick-rss.xml'
-    if os.path.exists(filename):
-        os.remove(filename)
-    with open(filename, 'w') as f:
-        f.write(generate_rss().strip())
-except:
-    print('Comick failed.')
+url = "https://api.comick.app/chapter/?page=1&order=new&accept_mature_content=true"
+data = requests.get(url).json()
+
+
+with open('comick.xml', 'w', encoding="utf-8") as f_out:
+    print(generate_rss(data), file=f_out)
